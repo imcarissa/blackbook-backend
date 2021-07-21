9e6caaa8d6737fe35c1631c7f57a63c3c0375784
imcarissa <carissa.sarreal@gmail.com>
2021-07-21 17:09:43 -0400

    removed typo in Group index

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::GroupsController < ApplicationController
 
     def index
         group = Group.all 
-        render json: @groups
+        render json: groups
     end
 
     def create

b8847128f8c6ed3df6e33914d41c6596b62f2228
imcarissa <carissa.sarreal@gmail.com>
2021-07-21 15:07:08 -0400

    variables are now local

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,27 +1,27 @@
 class GroupsController < ApplicationController
 
     def index
-        @group = Group.all 
+        group = Group.all 
         render json: @groups
     end
 
     def create
-        @group = Group.new(group_params)
+        group = Group.new(group_params)
         if @group.save
-            render json: @group
+            render json: group
         else
             render json: {error: 'Group not created'}
         end
     end
 
     def show
-        @group = Group.find(params[:id])
+        group = Group.find(params[:id])
         render json: @group
     end
     
     def destroy
-        @group = Group.find(params[:id])
-        @group.destory
+        group = Group.find(params[:id])
+        group.destory
     end
 
     private
